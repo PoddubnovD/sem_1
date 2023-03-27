2f3260e280091de4cc6992c5cd3cf1c214a21334
PoddubnovD <poddubnovd@yandex.ru>
2023-03-27 21:28:19 +0300

    add homework dz1

--- src/main/java/lesson1/MainClassFromLessonOne.java
+++ src/main/java/lesson1/MainClassFromLessonOne.java
@@ -3,49 +3,51 @@ package lesson1;
 import java.util.Scanner;
 
 public class MainClassFromLessonOne {
-    public static void main(String[] args) {
 
-//        exOne();
-
-    }
-
-    private static void exOne() {
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.printf("Привет, %s", name);
-        scanner.close();
-    }
-}
-
-//public class MainClassFromLessonOne {
-    public static void main(String[] args) {
-        //inputNameAndPrint();
-        size2();
-
-    }
-
-//    private static void inputNameAndPrint() {
-//        Scanner scanner = new Scanner(System.in);  // ввод переменных
-//        String name = scanner.nextLine();
-//        System.out.printf("Привет, %s", name); //форматированный вывод
-//        scanner.close();
+//    public static void main(String[] args) {
+//        //inputNameAndPrint();
+//        size2();
+//
 //    }
+//
+//    private static void size2() {
+//        int[] arr = new int[]{1, 1, 0, 1, 1, 1, 0, 1};
+//        int k = 0;
+//        int kmax = 0;
+//        for (int num : arr) {
+//
+//            if (num == 1) {
+//                k++;
+//                if (k > kmax) {
+//                    kmax = k;
+//                }
+//            } else {
+//                k = 0;
+//            }
+//        }
+//        System.out.printf("Большая последовательность %s", kmax);
+//    }
+//}
 
-    private static void size2() {
-        int[] arr = new int[]{1, 1, 0, 1, 1, 1,0,1};
-        int k = 0;
-        int kmax = 0;
-        for (int num : arr) {
-
-            if (num == 1) {
-                k++;
-                if (k > kmax) {
-                    kmax = k;
-                }
-            } else {
-                k = 0;
-            }
+        public static void transArr(int[] arr, int k) {
+            int i_f, i_t;
+            i_t = 0;
+            for (i_f = 0; i_f < arr.length; i_f++)
+                if (arr[i_f] != k)
+                    arr[i_t++] = arr[i_f];
+            for (i_f = i_t; i_f < arr.length; i_f++)
+                arr[i_f] = k;
         }
-        System.out.printf("Большая последовательность %s", kmax);
+
+        public static void main(String[] args) {
+            int[] b = { 3, 2, 2, 3 };
+            for (int x : b)
+                System.out.print(x + " ");
+            System.out.println();
+            transArr(b, 3);
+            for (int x : b)
+                System.out.print(x + " ");
+            System.out.println();
+        }
     }
-}
+
